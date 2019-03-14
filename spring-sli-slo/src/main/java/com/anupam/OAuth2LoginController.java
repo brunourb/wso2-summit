dceafa08e4c4e1ace562c815b1e48c31160ce873
anupamgogoi-wso2 <anupam@wso2.com>
2019-03-14 15:17:17 -0300

    changes

--- spring-sli-slo/src/main/java/com/anupam/OAuth2LoginController.java
+++ spring-sli-slo/src/main/java/com/anupam/OAuth2LoginController.java
@@ -5,11 +5,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class OAuth2LoginController {
     @GetMapping("/me")
     public String index(Model model,
@@ -18,6 +19,6 @@ public class OAuth2LoginController {
         model.addAttribute("userName", oauth2User.getName());
         model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
         model.addAttribute("userAttributes", oauth2User.getAttributes());
-        return "index";
+        return "me.html";
     }
 }
